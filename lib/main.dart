e169edcfc1dc5d8694019615ff1bb2b5f8dfcfee
Harshit Pathak <harshitpathak29@gmail.com>
2020-09-06 00:09:13 +0530

    App drawer included

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'dart:html';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/CustomAppBar.dart';
-
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -16,31 +13,43 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
       ),
       home: Scaffold(
-        backgroundColor: Colors.deepPurpleAccent,
-        appBar: CustomAppBar(),
-        body: Container(
-          margin: EdgeInsets.all(24),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(
-                offset: new Offset(-10, -10),
-                color: Colors.white,spreadRadius: -5,blurRadius: 24),
-              BoxShadow(
-                  offset: new Offset(10, 10),
-                  color: Colors.black,spreadRadius: -5,blurRadius: 24),
-              ],
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [Colors.deepPurple.withAlpha(255),
-                  Colors.deepPurpleAccent.withAlpha(255)]),
-          ),
+        drawer:Drawer(
+          child:ListView(
+            padding:EdgeInsets.zero,
+            children:<Widget>[
+              DrawerHeader(
+                child:Text('CONTENTS',),
+                decoration: BoxDecoration(
+                  color:Colors.black54,
+                ),
+              ),
+              ExpansionTile(
+                title:Text('Data Structures'),
+                children:<Widget>[Text('Arrays'),Text("Linked Lists"),Text("Stack"),Text('Queues'),Text('Graphs'),Text('Trees')]
+              ),
+              ExpansionTile(
+                  title:Text('Algorithms'),
+                  children:<Widget>[Text("Greedy Algorithm"),Text("Recursive Algorithm"),Text('Backtracking Algorithm'),
+                    Text('Divide and Conquer'),Text('Brute Force Algorithm')]
+              ),
+            ]
+          )
         ),
-      )
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+          centerTitle: true,
+          title: Text('Data Structures and Algorithm',),
+
+        ),
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: new BoxDecoration(
+            image:new DecorationImage(
+              image: new AssetImage('assets/images/bgimage1.jpg'), fit: BoxFit.cover,)),
+            ),
+          ),
     );
   }
 }
